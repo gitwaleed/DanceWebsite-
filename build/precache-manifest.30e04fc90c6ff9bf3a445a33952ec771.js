a2b2aa73b1db05aeedaabcbf716f8fbf3ae311b9
gitwaleed <waleedansari630@gmail.com>
2022-01-28 03:12:18 +0500

    Initial commit

---
+++ build/precache-manifest.30e04fc90c6ff9bf3a445a33952ec771.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb6b0b8a8888a10b83da51f89745d511",
+    "url": "/TextUtils/index.html"
+  },
+  {
+    "revision": "bd9566f40e1a4c37f37c",
+    "url": "/TextUtils/static/css/main.5caeb38d.chunk.css"
+  },
+  {
+    "revision": "5da4d59910867ec6fb96",
+    "url": "/TextUtils/static/js/2.cd8ee3dc.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/TextUtils/static/js/2.cd8ee3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47aee0090a49691be00",
+    "url": "/TextUtils/static/js/3.5b3a70da.chunk.js"
+  },
+  {
+    "revision": "bd9566f40e1a4c37f37c",
+    "url": "/TextUtils/static/js/main.5b0ac07c.chunk.js"
+  },
+  {
+    "revision": "7ddc86b1fee1decfb39a",
+    "url": "/TextUtils/static/js/runtime-main.24a316db.js"
+  }
+]);
